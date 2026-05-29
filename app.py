86a092c8a3ae687ce9e86c304b3e73684f53843c
DHANUSHKUMAR T <t.dhanu2216@gmail.com>
2026-05-29 17:49:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -603,4 +603,4 @@ def edit_student(student_id):
 # Entry Point
 # ─────────────────────────────────────────
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
